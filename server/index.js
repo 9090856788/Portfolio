b16466190747a01a44b2bdf1131f92e908e214ea
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-05-19 16:45:36 +0530

    added the package & configure it.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
+// import all package & files Section
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
+// configure enviromental variable 
+dotenv.config({ path: "./config/.env"});
+const PORT = process.env.PORT || 3000;
+
+// Configure Packages Section
 const app = express();
-const PORT = 3000;
-
-app.get(( req, res ) => {
-    res.send("Hello Backend Development");
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/temp/"
+}));
+app.use(cors({
+    origin: [process.env.ADMIN_DASHBOARD_URL, process.env.PORTFOLIO_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+app.use(cookieParser());
 
 app.listen(PORT, () => {
-    console.log(`Server running the port: ${PORT}`)
+    console.log(`Server running the PORT: ${PORT} ):`)
 });
