d5780382e8b4efa20b4224e9601bfa41e56bfdf7
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-05-21 00:47:50 +0530

    create a get Api for get all message from database

--- server/controller/messageController.js
+++ server/controller/messageController.js
@@ -2,7 +2,8 @@ import catchSynchError from "../middleware/catchAsyncError.js";
 import ErrorHandler from "../middleware/error.js";
 import { Message } from "../models/messageSchema.js";
 
-const sendMessage = catchSynchError(async(req, res, next) => {
+// send message post api
+export const sendMessage = catchSynchError(async(req, res, next) => {
     const  { senderName, subject, message } = req.body;
     if(!senderName || !subject || !message){
         return next(new ErrorHandler("Please fill the full form", 400))
@@ -17,4 +18,10 @@ const sendMessage = catchSynchError(async(req, res, next) => {
     });
 });
 
-export default sendMessage;
+export const getAllMessages = catchSynchError(async(req, res, next) => {
+    const messages = await Message.find(); 
+    res.status(200).json({
+        success: true,
+        messages,
+    });
+});
