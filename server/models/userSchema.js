e59ebdc1a20069f528388692d7054fcb9eb3f8af
9090856788 <kanhucharansahoo595@gmail.com>
2024-05-22 20:32:09 +0530

    change minor mistake

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -72,7 +72,7 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
 
 // generating json web Token
 userSchema.methods.generateJsonWebToken = function () {
-  return jwt.sign({ id: this.id }, process.env.JWT_SECRET_KEY, {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES,
   });
 };
