dc3d1157df20c7930ab0350a1d2cfb3fe96071fe
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-08-01 04:21:48 +0530

    created the required pages

---
+++ admin/src/pages/ManageProjects.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const ManageProjects = () => {
+  return (
+    <div>ManageProjects</div>
+  )
+}
+
+export default ManageProjects;
